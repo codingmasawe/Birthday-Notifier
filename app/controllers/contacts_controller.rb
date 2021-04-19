54f6de59cafda102b56e6d383797cc3115553034
codingmasawe <44496652+codingmasawe@users.noreply.github.com>
2021-04-19 19:15:51 +0200

    added birthday controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,19 @@
+class ContactsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'contacts/new'
+  get 'contacts/create'
+  get 'contacts/update'
+  get 'contacts/edit'
+  get 'contacts/update'
+  get 'contacts/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
