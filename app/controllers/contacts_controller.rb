be6a2bcc74661efd233814dbff32bcb3144c26b0
codingmasawe <44496652+codingmasawe@users.noreply.github.com>
2021-04-19 19:21:23 +0200

    added controller mehtods

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,14 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to @contact
+    else
+      render :new
   end
 
   def update
@@ -15,5 +21,11 @@ class ContactsController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :birthdate)
   end
 end
